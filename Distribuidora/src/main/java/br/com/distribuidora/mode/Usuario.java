0ccca00b386af482f7169fa65ed6f7b9710b2e53
Victor Henrique Amaral <54120724+victorhadn@users.noreply.github.com>
2020-09-30 22:22:06 -0300

    Update Usuario.java

--- Distribuidora/src/main/java/br/com/distribuidora/mode/Usuario.java
+++ Distribuidora/src/main/java/br/com/distribuidora/mode/Usuario.java
@@ -18,8 +18,10 @@ import lombok.Data;
 	public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String login;
 	private int senha;
+	public Long getID() { return id;}
 	/**
 	 * @return the login
 	 */
